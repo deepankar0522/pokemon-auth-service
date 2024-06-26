02d0ef0643cb6efca99dfee649e184a791112fd7
deepankaranand <deepravi0522@gmail.com>
2024-06-26 19:28:06 +0530

    Initial commit

---
+++ src/main/java/com/pokemon/auth/api/PokemonAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pokemon.auth.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokemon/auth/api/PokemonAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemon.auth.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
